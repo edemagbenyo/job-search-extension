8f9dd017964affee83aea9279aa64e1b038e5dca
edemagbenyo <edem.agbenyo@gmail.com>
2020-09-10 12:13:06 +0000

    extract into their own module

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,31 +1,16 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 const got = require('got');
 const cheerio = require('cheerio');
+const {remoteok} = require('./platforms/remoteok');
 
 exports.getLatestJobs = functions.https.onRequest(async (req, res) => {
-  const vgmUrl = 'https://www.indeed.com/jobs?q=Software%20Engineer';
+  let jobsList=[];
+  let userList = req.userList;
+  //Get the list of user preference sites
+  //do a for in 
+  let remoteokJobs = await remoteok(cheerio,got)
 
-  got(vgmUrl).then(response => {
-    const $ = cheerio.load(response.body);
-    //get jobs on the page
-    const jobs =  $('h2 a','.result');
-    let list = [];
-    // console.log($('h2 a','.result')[0].attribs.title)
-    for(let job in jobs){
-      if(jobs[job].attribs && 'title' in jobs[job].attribs ){
-        list.push(jobs[job].attribs.title)
-      }
-    }
-    return res.json({jobs:list})
-  }).catch(err => {
-    console.log(err);
-  });
+  return res.json([...remoteokJobs])
+  
 })
---
+++ functions/functions/platforms/remotive.js
@@ -0,0 +1,25 @@
+exports.remoteok = async function(cheerio, got){
+  const getJobs = await got("https://remoteok.io/")
+  
+  const response = await getJobs;
+   const $ = cheerio.load(response.body);
+   let list = [];
+   const body = $('body')
+   let jobs = body.find('tr.job td.company_and_position');
+   for(let job in jobs){
+     let td = jobs[job];
+     let info;
+     if(typeof td ==='object' && td.type=='tag' &&  td.name== 'td'){
+       let title = $(jobs[job]).children('h2').text()
+       let position = $(jobs[job]).find('a.preventLink');
+       let companyA = $(jobs[job]).find('a.companyLink');
+       let company = companyA.text()
+       let link = "https://remoteok.io" + position.attr('href')
+        info= {title,company,link}
+     }
+
+     list.push(info)
+   }
+   return list
+ 
+}
